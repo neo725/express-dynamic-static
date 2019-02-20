e77d151655de8a64444438b8e5595fb00c5f19eb
neo725 <neo725@gmail.com>
2019-02-20 14:12:15 +0800

    apply on child level search

--- scan-static-www.js
+++ scan-static-www.js
@@ -1,4 +1,9 @@
-const { statSync, lstatSync, readdirSync, existsSync } = require('fs')
+const {
+    statSync,
+    lstatSync,
+    readdirSync,
+    existsSync
+} = require('fs')
 const path = require('path')
 const Filehound = require('filehound')
 const Stopwatch = require("node-stopwatch").Stopwatch
@@ -9,72 +14,88 @@ const isDirectory = source => lstatSync(source).isDirectory()
 
 const getDirectories = (source) => Filehound.create().path(source).directory().depth(1).findSync()
 
-let _scan = function(queryPath, resolve, reject) {
+let _scan = function (queryPath, resolve, reject) {
     try {
-        console.log(`_scan : ${queryPath}`)
-        console.log(`__dirname (current path) : ${__dirname}`)
-
-        var stopwatch = Stopwatch.create()
-        stopwatch.start()
-
-        let dirs = getDirectories(queryPath)
-
-        stopwatch.stop()
-
-        console.log(`return dirs.length : ${dirs.length}`)
-        console.log(`getDirectories : ${stopwatch.elapsedMilliseconds} ms`)
-
         let outputs = []
 
-        dirs.forEach(dir => {
-            let _path = path.resolve(__dirname, dir)
-            //console.log(`_path : ${_path}`)
-            let currentPath = path.join(_path, 'www')
+        console.log(`__dirname (current path) : ${__dirname}`)
 
-            console.log(`current Path : ${currentPath}`)
+        let searchDirectories = (_queryPath, level, currentLevel) => {
+            if (currentLevel >= level) return
 
-            //if (existsSync(currentPath)) { // this is for file check, not directory
-            if (existsSync(currentPath) && isDirectory(currentPath)) {
-                
-                // get sub directives
-                console.log(`look into ${currentPath} (its a exists directory)`)
+            console.log(`_queryPath : ${_queryPath}`)
 
-                console.log(`basename : ${path.basename(_path)}`)
+            console.log(`level : ${level}`)
+            console.log(`currentLevel : ${currentLevel}`)
 
-                // check is root has default-pages that defined in config.js
-                let _config = require('./config')
-                let defaultPages = _config['default-pages'] || []
+            var stopwatch = Stopwatch.create()
+            stopwatch.start()
 
-                let defaultExists = false
+            let dirs = getDirectories(_queryPath)
 
-                defaultPages.forEach(pageName => {
-                    let checkPath = path.join(currentPath, pageName)
-                    if (existsSync(checkPath)) {
-                        defaultExists = true
+            stopwatch.stop()
+
+            console.log(`return dirs.length : ${dirs.length}`)
+            console.log(`getDirectories : ${stopwatch.elapsedMilliseconds} ms`)
+
+            dirs.forEach(dir => {
+                let _path = path.resolve(__dirname, dir)
+                //console.log(`_path : ${_path}`)
+                let currentPath = path.join(_path, 'www')
+
+                console.log(`current Path : ${currentPath}`)
+
+                //if (existsSync(currentPath)) { // this is for file check, not directory
+                if (existsSync(currentPath) && isDirectory(currentPath)) {
+
+                    // get sub directives
+                    console.log(`look into ${currentPath} (its a exists directory)`)
+
+                    console.log(`basename : ${path.basename(_path)}`)
+
+                    // check is root has default-pages that defined in config.js
+                    // >> /thchang/www
+                    let _config = require('./config')
+                    let defaultPages = _config['default-pages'] || []
+
+                    let defaultExists = false
+
+                    defaultPages.forEach(pageName => {
+                        let checkPath = path.join(currentPath, pageName)
+                        if (existsSync(checkPath)) {
+                            defaultExists = true
+                            return
+                        }
+                    })
+
+                    if (defaultExists) {
+                        outputs.push({
+                            name: path.basename(_path),
+                            path: currentPath
+                        })
+
                         return
                     }
-                })
 
-                if (defaultExists) {
-                    outputs.push({
-                        name: path.basename(_path),
-                        path: currentPath
-                    })
+                    // search 1st level child folder
+                    searchDirectories(currentPath, level, ++currentLevel)
                 }
-            }
-        })
+            })
+        }
+
+        searchDirectories(queryPath, 2, 0)
+
         resolve(outputs)
-    }
-    catch (ex) {
+    } catch (ex) {
         reject(ex)
 
         console.log(ex)
     }
 }
 
-module.exports = (queryPath) => {
-    console.log(`path : ${queryPath}`)
+module.exports = (_queryPath) => {
+    console.log(`path : ${_queryPath}`)
     return new Promise((resolve, reject) => {
-        _scan(queryPath, resolve, reject)
+        _scan(_queryPath, resolve, reject)
     })
 }
