675b423d294156f9a123e58f0f3519ad5f849f27
neo725 <neo725@gmail.com>
2019-02-20 17:17:15 +0800

    try psaux to fetch process list

--- server.js
+++ server.js
@@ -3,26 +3,33 @@
 require('colors')
 
 let ps = require('ps-node')
+let Psaux = require('psaux'),
+    psaux = Psaux()
 
 try {
 
-    ps.lookup({
-        command: 'node',
-        psargs: 'ux'
-    }, function (err, resultList) {
-        if (err) {
-            throw new Error(err);
-        }
+    // ps.lookup({
+    //     command: 'node',
+    //     psargs: 'aux'
+    // }, function (err, resultList) {
+    //     if (err) {
+    //         throw new Error(err);
+    //     }
 
-        let isNginxRunning = false;
+    //     let isNginxRunning = false;
 
-        resultList.forEach(function (process) {
-            if (process) {
-                console.log('PID: %s, COMMAND: %s, ARGUMENTS: %s', process.pid, process.command, process.arguments);
-            }
-        })
+    //     resultList.forEach(function (process) {
+    //         if (process) {
+    //             console.log('PID: %s, COMMAND: %s, ARGUMENTS: %s', process.pid, process.command, process.arguments);
+    //         }
+    //     })
+    // })
+    psaux.parsed(function (err, res) {
+        if (err) return console.error(err);
+        
+        console.log(res);
     })
-    
+
 } catch (ex) {
     console.log(ex)
 }
