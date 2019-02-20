cb5929a6b907be5756e1290770beb86942bb8c1c
neo725 <neo725@gmail.com>
2019-02-20 11:57:17 +0800

    rule update # 1

--- config.js
+++ config.js
@@ -1,5 +1,10 @@
 module.exports = {
     'path': './fake_folders',
     'port': 8010,
-
+    'default-pages': [
+        'index.html',
+        'index.htm',
+        'default.html',
+        'default.htm'
+    ]
 }
--- scan-static-www.js
+++ scan-static-www.js
@@ -7,7 +7,7 @@ const isDirectory = source => lstatSync(source).isDirectory()
 // const getDirectories = source =>
 //   readdirSync(source).map(name => path.join(source, name)).filter(isDirectory)
 
-const getDirectories = (source) => Filehound.create().path(source).directory().depth(1).findSync()
+const getDirectories = (source) => Filehound.create().path(source).directory().depth(0).findSync()
 
 let _scan = function(queryPath, resolve, reject) {
     try {
@@ -26,54 +26,48 @@ let _scan = function(queryPath, resolve, reject) {
 
         let outputs = []
 
-        // console.log('dirs :')
-        // console.log(dirs)
-
-        let checkIsWebFolder = (_) => {
-            let fullpath = path.join(_, 'www')
-
-            return existsSync(fullpath);
-        }
-
         dirs.forEach(dir => {
             let _path = path.resolve(__dirname, dir)
             //console.log(`_path : ${_path}`)
-            let fullpath = path.join(_path, 'www')
+            let currentPath = path.join(_path, 'www')
 
-            console.log(`fullpath : ${fullpath}`)
-            // console.log(`existsSync(fullpath) : ${existsSync(fullpath)}`)
-            // console.log(`isDirectory(fullpath) : ${isDirectory(fullpath)}`)
+            console.log(`Current Path : ${currentPath}`)
 
-            //if (existsSync(fullpath)) { // this is for file check, not directory
-            if (existsSync(fullpath) && isDirectory(fullpath)) {
+            //if (existsSync(currentPath)) { // this is for file check, not directory
+            if (existsSync(currentPath) && isDirectory(currentPath)) {
                 // current location is under user's www folder
                 // // check is there has a www file in sub directory
                 // let folder_array = dir.split('\\')
                 // let folder = folder_array[folder_array.length - 1]
-
 
                 // // outputs.push({
                 // //     name: folder,
                 // //     path: _path
                 // // })
                 // get sub directives
-                console.log(`look into ${fullpath}`)
-                let subs = getDirectories(fullpath)
-                if (subs && subs.length > 0) {
-                    subs.forEach(sub => {
-                        console.log(`sub : ${sub}`)
+                console.log(`look into ${currentPath} (its a exists directory)`)
+
+                console.log(`basename : ${path.basename(_path)}`)
+
+                // check is root has default-pages that defined in config.js
+                let _config = require('./config')
+                let defaultPages = _config['default-pages'] || []
+
+                let defaultExists = false
+
+                defaultPages.forEach(pageName => {
+                    let checkPath = path.join(currentPath, pageName)
+                    if (existsSync(checkPath)) {
+                        defaultExists = true
+                        return
+                    }
+                })
+
+                if (defaultExists) {
+                    // outputs.push({
                         
-                        let folder = path.basename(sub)
-                        
-                        //let folder = sub.split('')
-                        if (isDirectory(sub)) {
-                            
-                            outputs.push({
-                                name: folder,
-                                path: sub
-                            })
-                        }
-                    })
+                    // })
+                    console.log(`defaultExists !`)
                 }
             }
         })
